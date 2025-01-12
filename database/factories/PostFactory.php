a86f1d42a27e0aec5c85b06e91dade6cbeafb1a0
Haja N-Lanto <nirilantohajatiana032@gmail.com>
2025-01-12 22:42:58 +0300

    Update PostFactory.php

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+use function PHPUnit\Framework\fileExists;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -20,72 +23,20 @@ class PostFactory extends Factory
     {
         $title = fake()->unique()->words(asText: true);
         $content = fake()->paragraphs(mt_rand(3, 5), true);
-
-        //todo Match each covers by category
-        // ? Get one category name randomly
-        $category = Category::inRandomOrder()->first();
-
-        // ? Get the cover from the "addCover" function depending on the selected category
-        $cover = $this->addCover($category->name, null);
-
+        $cover = glob(storage_path('app/public/img/pre_save/*.*'));
+        $ok = array_rand($cover, 5);
+        if ($ok) {
+            dd($ok);
+            // Storage::disk('public')->copy('img/pre_save', 'img/uploads/posts/' . array_rand($cover));
+        }
 
         return [
             'title' => $title,
-            'category_id' => $category,
+            'category_id' => fake()->randomElement(Category::all()),
             'slug' => Str::slug($title),
-            'cover' => 'img/uploads/posts/' . $cover . '.jpg',
+            'cover' => fake()->image(storage_path('app/public/img/uploads/posts/'), category: 'post', fullPath: false, word: 'cover'),
             'excerpt' => Str::limit($content),
             'content' => $content
         ];
-    }
-
-    /**
-     * todo Create a list of different covers
-     *
-     * @return array
-     */
-    public function coversList(): array
-    {
-        return [
-            'Politiques' => ['politiques-1', 'politiques-2'],
-            'Economies' => ['économies-1', 'économies-2'],
-            'Sports' => ['sports-1', 'sports-2'],
-            'Environnements' => ['environnements-1', 'environnements-2'],
-            'Cultures' => ['cultures-1', 'cultures-2']
-        ];
-    }
-
-    /**
-     * todo Create a cover according to the selected category
-     *
-     * @param string $categoryName
-     * @param [type] $cover
-     * @return string
-     */
-    public function addCover(string $categoryName, $cover): string
-    {
-        // ? Add a  cover depending the selected category
-        $covers = $this->coversList();
-
-        switch ($categoryName) {
-            case "Politiques":
-                return $cover = $covers["Politiques"][mt_rand(0, 1)];
-                break;
-            case "Economies":
-                return $cover = $covers["Economies"][mt_rand(0, 1)];
-                break;
-            case "Sports":
-                return $cover = $covers["Sports"][mt_rand(0, 1)];
-                break;
-            case "Environnements":
-                return $cover = $covers["Environnements"][mt_rand(0, 1)];
-                break;
-            case "Cultures":
-                return $cover = $covers["Cultures"][mt_rand(0, 1)];
-                break;
-            default:
-                return $cover === 'default';
-                break;
-        }
     }
 }
