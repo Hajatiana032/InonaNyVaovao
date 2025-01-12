67819ef1962c9be9bb8efd9e76c7bacedee09b3c
Haja N-Lanto <nirilantohajatiana032@gmail.com>
2025-01-12 23:08:43 +0300

    Add french language

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,9 +5,7 @@ namespace Database\Factories;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Storage;
 
-use function PHPUnit\Framework\fileExists;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -23,18 +21,13 @@ class PostFactory extends Factory
     {
         $title = fake()->unique()->words(asText: true);
         $content = fake()->paragraphs(mt_rand(3, 5), true);
-        $cover = glob(storage_path('app/public/img/pre_save/*.*'));
-        $ok = array_rand($cover, 5);
-        if ($ok) {
-            dd($ok);
-            // Storage::disk('public')->copy('img/pre_save', 'img/uploads/posts/' . array_rand($cover));
-        }
+
 
         return [
             'title' => $title,
             'category_id' => fake()->randomElement(Category::all()),
             'slug' => Str::slug($title),
-            'cover' => fake()->image(storage_path('app/public/img/uploads/posts/'), category: 'post', fullPath: false, word: 'cover'),
+            'cover' => fake()->fileExtension('jpg'),
             'excerpt' => Str::limit($content),
             'content' => $content
         ];
